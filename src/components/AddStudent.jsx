967d06edd79969c21ee1d15452f692d0b4fe3491
bristow17 <bristowgrorge17@gmail.com>
2024-06-07 14:28:05 +0530

    till routing

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddStudent = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <lable className="label form-label">Name</lable>
+                        <input type="text" className="input form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="label form-label">Admission No</label>
+                        <input type="text" className="input form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="label form-label">Roll No</label>
+                        <input type="text" className="input form-control" />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="label form-label">Parent Name</label>
+                        <input type="text" className="input form-control" />
+
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <label htmlFor="" className="label form-label">College Name</label>
+                        <textarea name="" id="" className="form-control"></textarea>
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="label form-label">DOB</label>
+                        <input type="date" name="" id="" className="form-control" />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="label form-label">Email</label>
+                        <input type="text" className="input form-control" />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="label form-label">Password</label>
+                        <input type="text" className="input form-control" />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="label form-label">Confirm Password</label>
+                        <input type="text" className="input form-control" />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <button className="btn btn-primary">REGISTER</button>
+                    </div>
+                </div>
+
+                </div>
+            </div>
+        </div>
+    
+    </div>
+  )
+}
+
+export default AddStudent
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,191 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6SBFO_34s3mUw1zz2SuAVXn83OArtd8D9GQ&s" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://assets.vogue.com/photos/6618856b698b651e8adffe20/master/pass/Beauty-Haare-Margot-Robbie-GettyImages-2081514563.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://hips.hearstapps.com/hmg-prod/images/henry-cavill-6455184b166f4.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://i.pinimg.com/736x/5c/7f/73/5c7f7336fc8b556010babfd01f8c5c72.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6SBFO_34s3mUw1zz2SuAVXn83OArtd8D9GQ&s" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://assets.vogue.com/photos/6618856b698b651e8adffe20/master/pass/Beauty-Haare-Margot-Robbie-GettyImages-2081514563.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://hips.hearstapps.com/hmg-prod/images/henry-cavill-6455184b166f4.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://i.pinimg.com/736x/5c/7f/73/5c7f7336fc8b556010babfd01f8c5c72.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6SBFO_34s3mUw1zz2SuAVXn83OArtd8D9GQ&s" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://assets.vogue.com/photos/6618856b698b651e8adffe20/master/pass/Beauty-Haare-Margot-Robbie-GettyImages-2081514563.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://hips.hearstapps.com/hmg-prod/images/henry-cavill-6455184b166f4.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://i.pinimg.com/736x/5c/7f/73/5c7f7336fc8b556010babfd01f8c5c72.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6SBFO_34s3mUw1zz2SuAVXn83OArtd8D9GQ&s" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://assets.vogue.com/photos/6618856b698b651e8adffe20/master/pass/Beauty-Haare-Margot-Robbie-GettyImages-2081514563.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://hips.hearstapps.com/hmg-prod/images/henry-cavill-6455184b166f4.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://i.pinimg.com/736x/5c/7f/73/5c7f7336fc8b556010babfd01f8c5c72.jpg" class="card-img-top" alt="..." height="350px"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ViewAll
